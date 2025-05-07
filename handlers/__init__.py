3a79313843ce51747a00d8c494234ed5f79b6276
Вячеслав Львов <zigatul@MacBook-Air-Vaceslav.local>
2025-05-07 23:26:27 +0300

    Fix handlers package exports: add start_cmd to __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,3 +1,5 @@
 from .start import start_cmd
 from .nomination import nomination_handler
 from .soundtrack import soundtrack_handler
+
+__all__ = ["start_cmd", "nomination_handler", "soundtrack_handler"]
